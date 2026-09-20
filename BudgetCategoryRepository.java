a0ea5ffdbef0b11dec8b27f9b86380d22bc02caa
onzeem <eso30@pitt.edu>
2026-09-19 23:44:41 -0400

    Delete BudgetCategoryRepository.java

--- BudgetCategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface BudgetCategoryRepository extends JpaRepository<BudgetCategory, Long> {
-    List<BudgetCategory> findByBudgetId(Long budgetId);
-}
